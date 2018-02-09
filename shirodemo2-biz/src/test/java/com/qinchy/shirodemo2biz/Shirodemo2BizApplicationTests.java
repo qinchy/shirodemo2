03eae6314907aa0a3fac2f80df3e8f0ddcb888d7
qinchy <qinchy@msn.com>
2018-02-09 10:27:44 +0800

    first commit

---
+++ shirodemo2-biz/src/test/java/com/qinchy/shirodemo2biz/Shirodemo2BizApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qinchy.shirodemo2biz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Shirodemo2BizApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
